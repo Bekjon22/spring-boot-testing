0af72b7fc00af849d111125322043f50f6500a6b
BekjonBakhromov <bakhromovbekjon2211@gmail.com>
2022-04-04 16:40:04 +0500

    initial commit

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -10,7 +11,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 
@@ -61,7 +66,29 @@ class StudentServiceTest {
     }
 
     @Test
+    void willThrowWhenEmailIsTaken() {
+        //given
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+
+        given(studentRepository.selectExistsEmail(student.getEmail()))
+                .willReturn(true);
+
+        //when
+        //then
+      assertThatThrownBy(() -> underTest.addStudent(student))
+              .isInstanceOf(BadRequestException.class)
+              .hasMessageContaining("Email " + student.getEmail() + " taken");
+
+      verify(studentRepository,never()).save(any());
+    }
+
+    @Test
+    @Disabled
     void deleteStudent() {
-        //kk
+
     }
 }
